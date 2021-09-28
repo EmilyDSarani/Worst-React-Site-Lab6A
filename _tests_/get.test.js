f3d7ffa0eeef07ae50495aee2537fcef11c28562
EmilyDSarani <emily.sarani@gmail.com>
2021-09-28 16:20:29 -0500

    tests

---
+++ _tests_/get.test.js
@@ -0,0 +1,83 @@
+const { app } = require('../server.js');
+const supertest = require('supertest');
+const { describe } = require('yargs');
+const request = supertest(app);
+
+describe('app routes',() =>{
+it('the avatar-characters endpoint will return all of the avatar characters',async()=>{
+    const data =
+    [
+        {
+            id: 1,
+            name:'Aang',
+            element: 'Air',
+            img: 'https://static.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003',
+            title: 'Avatar' 
+           }, 
+           {
+            id: 2,
+            name:'Zuko',
+            element: 'Fire',
+            img: 'https://i.redd.it/jcyddoyzeo761.jpg',
+            title: 'Prince' 
+        }, 
+        {
+            id: 3,
+            name:'Katara',
+            element: 'Water',
+            img: 'http://pm1.narvii.com/6078/caabea330dc68f44a3f855606f897516ec582759_00.jpg',
+            title: 'Healer' 
+        }, 
+        {
+            id: 4,
+            name:'Sokka',
+            element: 'None',
+            img: 'https://i.pinimg.com/originals/08/ca/e8/08cae8f53ebfd473b1b3133b0138d9cd.jpg',
+            title: 'Plan-Guy' 
+        }, 
+        {
+            id: 5,
+            name:'Toph',
+            element: 'Earth',
+            img: 'http://images4.wikia.nocookie.net/__cb20081225191856/avatar/images/2/2e/Toph.png',
+            title: 'Blind-Bandit' 
+        }, 
+        {
+            id: 6,
+            name:'Suki',
+            element: 'None',
+            img: 'https://comicvine1.cbsistatic.com/uploads/scale_super/11138/111385676/7019867-suki.jpg',
+            title: 'Kyoshi-Warrior' 
+        }, 
+        {
+            id: 7,
+            name:'Appa',
+            element: 'Air',
+            img: 'https://static.wikia.nocookie.net/avatar/images/b/b9/Lent%C3%A4v%C3%A4_Appa.png/revision/latest?cb=20150213084632&path-prefix=fi',
+            title: 'Bison' 
+        }, 
+        {
+            id: 8,
+            name:'Momo',
+            element: 'Air',
+            img: 'https://static.wikia.nocookie.net/avatar/images/8/80/Momo_staring.png/revision/latest/top-crop/width/360/height/360?cb=20200802080013',
+            title: 'Lemur' 
+        }, 
+    ]
+})
+
+})
+
+describe('app routes',() =>{
+    it('the avatar-characters endpoint will return Sokka',async()=>{
+        const data =
+        [{
+            id: 4,
+            name:'Sokka',
+            element: 'None',
+            img: 'https://i.pinimg.com/originals/08/ca/e8/08cae8f53ebfd473b1b3133b0138d9cd.jpg',
+            title: 'Plan-Guy' 
+        }, ]
+    })
+
+})
