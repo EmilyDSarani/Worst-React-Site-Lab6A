58884139e2ada382eba264ffb7a514a2f3b5de99
EmilyDSarani <emily.sarani@gmail.com>
2021-09-28 17:31:02 -0500

    passing again

--- served.js
+++ served.js
@@ -3,15 +3,13 @@ const {myAvatar} = require('./data.js');
 const express= require('express');
 const app = express();
 
-const port = 3000
 
-port = process.env.PORT || 3000;
 
-app.get('/avatar-characters', (req,res)=>{
+app.get('/avatar-characters', (req, res) => { 
     res.json(myAvatar)
 });
 
-app.get('/avatar-characters:id', (req,res) => {
+app.get('/avatar-characters:id', (req, res) => {
      myAvatar.find(character => character.id === Number(req.params.id));
     res.json({myId:req.params.id});
     });
